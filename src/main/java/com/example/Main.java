6774ce81b708752a03020005ca1db069236f4f9d
Alexander Stenders <alexander.stenders@iths.se>
2025-09-30 22:47:38 +0200

    Testing out args and see if switch picks it's content up

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,8 +2,24 @@ package com.example;
 
 import com.example.api.ElpriserAPI;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
+        for (int i = 0; i < args.length;i++) {
+            switch (args[i]) {
+                case "--zone" -> System.out.println("zone selected");
+                case "SE2" -> System.out.println("SE2 selected");
+                case "--date" -> System.out.println("date selected");
+                case "--sorted" -> System.out.println("sorted selected");
+            }
+        }
+
+        LocalDate testDate = LocalDate.now();
+
     }
 }
