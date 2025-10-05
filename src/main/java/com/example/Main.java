e5fa552f1cc7a70b551502eaa8cfec529efd61f0
Alexander Stenders <alexander.stenders@iths.se>
2025-10-05 22:16:23 +0200

    Fixade showHelp_withHelpFlag() testet.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,16 +4,48 @@ import com.example.api.ElpriserAPI;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
+
+        String[] helpMenu = {"--zone","--date","--charging","--sorted","SE1","SE2","SE3","SE4"};
+
+
+        // TODO: if args have length 1, it must include: --help
+        // TODO: if args have length 4 or more, it must include: --zone , SE? , --date , 20??-??-??
+        // TODO: if args have length 5, it must include: --sorted
+        // TODO: if args have length 6, it must include: --charging , ?h
+        // TODO: if any other length, it must be set as invalid.
+
+        // TODO: SE1-SE4 & 2h-4h-8h is the only valid options for that catagory
+        // TODO: Ska includera om det saknas --date --zone / fel SE? / fel datum / inget data includerat i elpriserAPI
+
+        for(String conent: args) {
+            if(args[0].equalsIgnoreCase("--help")) {
+                for(String allHelpMenuOptions: helpMenu){
+                    System.out.println(Arrays.toString(helpMenu));
+                }
+                break;
+            }
+        }
+
+
         for (int i = 0; i < args.length;i++) {
             switch (args[i]) {
-                case "--zone" -> System.out.println("zone selected");
+                case "SE1" -> System.out.println("SE1 selected");
                 case "SE2" -> System.out.println("SE2 selected");
+                case "SE3" -> System.out.println("SE3 selected");
+                case "SE4" -> System.out.println("SE4 selected");
+                case "2h" -> System.out.println("2h selected");
+                case "4h" -> System.out.println("4h selected");
+                case "8h" -> System.out.println("8h selected");
+                case "--zone" -> System.out.println("zone selected");
+                case "--charging" -> System.out.println("charging selected");
+                case "--help" -> System.out.println("help selected");
                 case "--date" -> System.out.println("date selected");
                 case "--sorted" -> System.out.println("sorted selected");
             }
